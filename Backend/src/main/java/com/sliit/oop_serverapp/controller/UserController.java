9adfb0b866bbde102c8488e62ccc6bcb45d32a09
Kodagoda O.I <it25103206@my.sliit.lk>
2026-05-15 18:55:08 +0530

    [C1] - User Repo Changes

--- Backend/src/main/java/com/sliit/oop_serverapp/controller/UserController.java
+++ Backend/src/main/java/com/sliit/oop_serverapp/controller/UserController.java
@@ -8,11 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * OOP Concept: Encapsulation & Abstraction
- * UserController encapsulates user and authentication operations.
- * It abstracts security and profile logic via the UserService.
- */
 @RestController
 @RequestMapping("/User")
 @CrossOrigin
@@ -24,30 +19,11 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<UserDTO> login(@RequestBody UserDTO loginDTO) {
         UserDTO user = userService.login(loginDTO);
+
         if (user != null) {
             return ResponseEntity.ok(user);
         }
         return ResponseEntity.status(401).build();
     }
-
-    @GetMapping
-    public List<UserDTO> getAll() {
-        return userService.getAllUsers();
-    }
-
-    @PostMapping("/Add")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(userService.createUser(userDTO));
-    }
-
-    @PutMapping("/Update")
-    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(userService.updateUser(userDTO));
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable Integer id) {
-        userService.deleteUser(id);
-        return ResponseEntity.ok("User Deleted Successfully");
-    }
+    
 }
