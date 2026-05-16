fe3ee3b8cf62cb49250fc1c412bba67835a98603
IT23321168 <it23321168@my.sliit.lk>
2026-05-16 13:47:07 +0530

    Review Part - Updated CRUD Operation

--- Backend/src/main/java/com/sliit/oop_serverapp/controller/ReviewController.java
+++ Backend/src/main/java/com/sliit/oop_serverapp/controller/ReviewController.java
@@ -30,5 +30,19 @@ public class ReviewController {
     public List<ReviewDTO> getByBook(@PathVariable Integer bookId) {
         return reviewService.getReviewsByBookId(bookId);
     }
-    
+    @PostMapping("/Add")
+    public ResponseEntity<ReviewDTO> addReview(@RequestBody ReviewDTO reviewDTO) {
+        return ResponseEntity.ok(reviewService.createReview(reviewDTO));
+    }
+
+    @PutMapping("/Update")
+    public ResponseEntity<ReviewDTO> updateReview(@RequestBody ReviewDTO reviewDTO) {
+        return ResponseEntity.ok(reviewService.updateReview(reviewDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteReview(@PathVariable Integer id) {
+        reviewService.deleteReview(id);
+        return ResponseEntity.ok("Review Deleted Successfully");
+    }
 }
