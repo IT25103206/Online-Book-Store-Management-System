3464d7d924465a96ca435b6e6558817c8128b110
IT25101591-Withanage V.T <it25101591@my.sliit.lk>
2026-05-18 01:53:32 +0530

    Updated Order Controller

--- Backend/src/main/java/com/sliit/oop_serverapp/controller/OrderController.java
+++ Backend/src/main/java/com/sliit/oop_serverapp/controller/OrderController.java
@@ -27,7 +27,11 @@ public class OrderController {
         return orderService.getAllOrders();
     }
 
-
+    @PostMapping("/Add")
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        return ResponseEntity.ok(orderService.createOrder(orderDTO));
+    }
+    
     @PutMapping("/Update")
     public ResponseEntity<OrderDTO> updateOrder(@RequestBody OrderDTO orderDTO) {
         return ResponseEntity.ok(orderService.updateOrder(orderDTO));
