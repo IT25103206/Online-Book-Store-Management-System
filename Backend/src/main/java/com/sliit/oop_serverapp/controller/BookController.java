6ad802a4b12429af2d57f3a835c39c88634e1c40
IT25103255 <it25103255@my.sliit.lk>
2026-05-17 14:27:08 +0530

    Updated part of the backend code in the book controller

--- Backend/src/main/java/com/sliit/oop_serverapp/controller/BookController.java
+++ Backend/src/main/java/com/sliit/oop_serverapp/controller/BookController.java
@@ -16,28 +16,9 @@ public class BookController {
      @Autowired
     private BookService bookService;
 
-    @PostMapping("/Upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
-        try {
-            if (file.isEmpty()) return ResponseEntity.badRequest().body("File is empty");
-            
-            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-            byte[] bytes = file.getBytes();
-            
-            // SAVE TO EXTERNAL PATH (More robust path construction for Windows/Linux)
-            Path uploadPath = Paths.get(System.getProperty("user.home"), "lumina_books", "uploads");
-            if (!Files.exists(uploadPath)) {
-                Files.createDirectories(uploadPath);
-            }
-            
-            Path path = uploadPath.resolve(fileName);
-            Files.write(path, bytes);
-            
-            return ResponseEntity.ok("http://localhost:8080/uploads/" + fileName);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("Upload failed on server: " + e.toString());
-        }
+    
+
+   
         
     }
     @GetMapping
