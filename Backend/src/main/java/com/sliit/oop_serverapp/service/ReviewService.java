c2ebcdab905ba4bdddf85c8f4759343feb3c6dd9
IT23321168 <it23321168@my.sliit.lk>
2026-05-18 10:12:01 +0530

    Edit Interface Review

--- Backend/src/main/java/com/sliit/oop_serverapp/service/ReviewService.java
+++ Backend/src/main/java/com/sliit/oop_serverapp/service/ReviewService.java
@@ -3,10 +3,4 @@ package com.sliit.oop_serverapp.service;
 import com.sliit.oop_serverapp.dto.ReviewDTO;
 import java.util.List;
 
-public interface ReviewService {
-    List<ReviewDTO> getAllReviews();
-    List<ReviewDTO> getReviewsByBookId(Integer bookId);
-    ReviewDTO createReview(ReviewDTO reviewDTO);
-    ReviewDTO updateReview(ReviewDTO reviewDTO);
-    void deleteReview(Integer id);
-}
+
